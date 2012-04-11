397a5f7b2d0d1b20bd4af5aeff68e9ba6d8e0596
prrekapaee9f <prasant.rekapalli@airbus.com>
2012-04-11 12:22:11 +0530

    Update 'G' character of Bala

--- summary.c
+++ summary.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-   printf("Bala is awesome, brave, cool, daring, exceptional, fearless\n");
+   printf("Bala is awesome, brave, cool, daring, exceptional, fearless, generous\n");
    return 1;
 }
