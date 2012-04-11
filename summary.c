475b456669aa189394aaa6c7fb3244230d7087d2
prrekapaee9f <prasant.rekapalli@airbus.com>
2012-04-11 15:54:52 +0530

    Update 'K' character of Bala

--- summary.c
+++ summary.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-   printf("Bala is awesome, brave, cool, daring, exceptional, fearless, generous, humble, incredible\n");
+   printf("Bala is awesome, brave, cool, daring, exceptional, fearless, generous, humble, incredible, knowledgeable\n");
    return 1;
 }
